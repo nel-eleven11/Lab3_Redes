2e45b69a0e026f6f46283eb43c8d85a9b9050298
ElrohirGT <elrohirgt@gmail.com>
2025-08-16 15:55:05 -0600

    feat: initialized go module and flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A basic multiplatform flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    # System types to support.
+    supportedSystems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
+
+    # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+    # Nixpkgs instantiated for supported system types.
+    nixpkgsFor = forAllSystems (system: import nixpkgs {inherit system;});
+  in {
+    devShells = forAllSystems (system: let
+      pkgs = nixpkgsFor.${system};
+    in {
+      default = pkgs.mkShell {
+        packages = [
+          pkgs.go
+        ];
+      };
+    });
+  };
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nel-eleven11/Lab3_Redes
+
+go 1.24.5
