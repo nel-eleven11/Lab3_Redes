5c14f20ee75b87a580c5beb8767c0968a013f574
ElrohirGT <elrohirgt@gmail.com>
2025-08-21 21:37:15 -0600

    feat: Added main [incomplete]

--- Lab3_Parte2/main.go
+++ Lab3_Parte2/main.go
@@ -2,17 +2,25 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"flag"
 	"log"
 	"os"
+	"sync"
 
 	"github.com/joho/godotenv"
 	"github.com/redis/go-redis/v9"
 )
 
-var ctx = context.Background()
+const NODE_ID = "nodo6"
 
-func ExampleClient() {
+var FULL_NODE_ID = formatRedisChannel(NODE_ID)
+
+func formatRedisChannel(nodeId string) string {
+	return "sec20.topologia2." + nodeId
+}
+
+func main() {
 	var nodeType string
 	flag.StringVar(&nodeType, "t", "flood", "Node Type (flood or lsr)")
 	flag.Parse()
@@ -36,4 +44,95 @@ func ExampleClient() {
 		Addr:     redisAddr,
 		Password: redisPwd,
 	})
+
+	// This channel only receives `ProtoclMsg` types!
+	senderChan := make(chan any, 10)
+	defer close(senderChan)
+	// This channel only receives payloads defined on `payloads.go`
+	receiverChan := make(chan any, 10)
+	defer close(receiverChan)
+
+	ctx, cancelCtx := context.WithCancel(context.Background())
+	defer cancelCtx()
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		// Receive messages from redis
+		pubsub := rdb.Subscribe(ctx, FULL_NODE_ID)
+		defer pubsub.Close()
+		redisReceiveChannel := pubsub.Channel()
+
+		for {
+			select {
+			case redisMsg := <-redisReceiveChannel:
+				msg := redisMsg.Payload
+				var initMsg ProtocolMsg[InitPayload]
+				var messageMsg ProtocolMsg[MessagePayload]
+				var doneMsg ProtocolMsg[DonePayload]
+
+				if err := json.Unmarshal([]byte(msg), &initMsg); err != nil {
+					// Handle init msg
+					receiverChan <- initMsg.Payload
+					continue
+				} else if err = json.Unmarshal([]byte(msg), &messageMsg); err != nil {
+					// Handle message msg
+					receiverChan <- messageMsg.Payload
+				} else if err = json.Unmarshal([]byte(msg), &doneMsg); err != nil {
+					// Handle done msg
+					receiverChan <- doneMsg.Payload
+				} else {
+					log.Panicf("Message is not on the correct format!\n%s", err)
+				}
+
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+
+	wg.Add(1)
+	go func() {
+		wg.Done()
+		neighboursId := []string{}
+
+		for {
+			select {
+			case receivedMsg := <-senderChan:
+				if msg, is := receivedMsg.(ProtocolMsg[InitPayload]); is {
+					jsonString, err := json.Marshal(msg)
+					if err != nil {
+						log.Panicf("Failed to marshal json: %#v", msg)
+					}
+
+					for _, nodeId := range neighboursId {
+						rdb.Publish(ctx, formatRedisChannel(nodeId), jsonString)
+					}
+				} else if msg, is := receivedMsg.(ProtocolMsg[MessagePayload]); is {
+					msg.Payload.Origin = NODE_ID
+					jsonString, err := json.Marshal(msg)
+					if err != nil {
+						log.Panicf("Failed to marshal json: %#v", msg)
+					}
+
+					rdb.Publish(ctx, formatRedisChannel(msg.Payload.Destination), jsonString)
+				} else if msg, is := receivedMsg.(ProtocolMsg[DonePayload]); is {
+					jsonString, err := json.Marshal(msg)
+					if err != nil {
+						log.Panicf("Failed to marshal json: %#v", msg)
+					}
+
+					for _, nodeId := range neighboursId {
+						rdb.Publish(ctx, formatRedisChannel(nodeId), jsonString)
+					}
+				} else {
+					log.Panicf("Received invalid ProtocolMsg!\n%#v", receivedMsg)
+				}
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
 }
---
+++ Lab3_Parte2/payloads.go
@@ -0,0 +1,53 @@
+package main
+
+type ProtocolMsg[T any] struct {
+	Type    string `json:"type"`
+	Payload T      `json:"payload"`
+}
+
+// Mensaje cuando el <nodo 1> se presenta ante sus vecinos
+// {
+//   "type": "init",
+//   "payload": {
+//     "whoAmI": "<nodo 1>",
+//     "neighbours": {
+//       "<nodo 1>": 5,
+//       "<nodo 2>": 10
+//     }
+//   }
+// }
+
+type InitPayload struct {
+	WhoAmI     string         `json:"whoAmI"`
+	Neighbours map[string]int `json:"neighbours"`
+}
+
+// Mensaje enviado cuando el <nodo 1> necesita enviar un mensaje al <nodo 2>
+// {
+//   "type": "message",
+//   "payload": {
+//     "origin": "<nodo 1>",
+//     "destination": "<nodo 2>",
+//     "ttl": 5,
+//     "content": "asdflkjalsdkfjalksjdf"
+//   }
+// }
+
+type MessagePayload struct {
+	Origin      string `json:"origin"`
+	Destination string `json:"destination"`
+	Ttl         uint   `json:"ttl"`
+	Content     string `json:"content"`
+}
+
+// Mensaje enviado cuando ya terminó de calcular sus tablitas
+// {
+//   "type": "done",
+//   "payload": {
+//     "whoAmI": "<nodo 1>"
+//   }
+// }
+
+type DonePayload struct {
+	WhoAmI string `json:"whoAmI"`
+}
